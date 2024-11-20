e12be878e3a656641ab9f874fd808d39feb044bb
Ahmed EL SAADAWY <94537516+AhmedElSaa@users.noreply.github.com>
2024-11-20 16:54:06 +0100

    Update App.java

--- src/main/java/com/test/HelloWorldMaven/App.java
+++ src/main/java/com/test/HelloWorldMaven/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Coucou c'est moi 4" );
+        System.out.println( "Coucou c'est moi 5" );
     }
 }
